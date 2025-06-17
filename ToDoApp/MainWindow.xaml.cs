12cf545fd5df3b862c877997b02184911a4a27cf
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-06-18 00:10:28 +0300

    Регистрация и авторизация теперь работают

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
-
+using System.IO;
+using System.Data.SQLite;
 
 namespace ToDoApp
 {
@@ -14,16 +14,51 @@ namespace ToDoApp
         }
         private void Registration_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Переход к регистрации");
+            RegistrationWindow regWindow = new RegistrationWindow();
+            regWindow.Show();
         }
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Вход в приложений");
-        }
+            string username = usernameBox.Text;
+            string password = passwordBox.Password;
 
-        private void TextBox_TextInput(object sender, TextCompositionEventArgs e)
-        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Введите логин и пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
 
+            string dbPath = "users.db";
+            string connectionString = $"Data Source={dbPath}";
+
+            if (!File.Exists(dbPath))
+            {
+                MessageBox.Show("База данных не найдена.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @username AND Password = @password";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    long count = (long)command.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Вход выполнен успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный логин или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
         }
     }
 }
